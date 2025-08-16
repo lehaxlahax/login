3b0d9c9c4d442e0ef3c5edb6eab64fe4456fc1e9
lehaxlahax <lehaxlahax@gmail.com>
2025-08-16 14:37:53 +0100

    Add files via upload

---
+++ Log in/script.js
@@ -0,0 +1,23 @@
+// Attend que tout le contenu de la page soit chargé
+window.addEventListener('load', () => {
+
+    // Sélectionne les éléments de la page
+    const splashScreen = document.getElementById('splash-screen');
+    const loginPage = document.getElementById('login-page');
+
+    // Définit un minuteur de 3000 millisecondes (3 secondes)
+    setTimeout(() => {
+        // Commence l'animation de fondu pour l'écran de chargement
+        splashScreen.style.opacity = '0';
+
+        // Affiche la page de connexion
+        loginPage.style.display = 'flex';
+
+        // Après la fin de l'animation de fondu (0.5s), cache complètement l'écran de chargement
+        splashScreen.addEventListener('transitionend', () => {
+            splashScreen.style.display = 'none';
+        });
+
+    }, 3000); // Délai de 3 secondes
+
+});
